686487e87deda571eb70863c9bdbf5dbbee3e9c7
Vicryl <wariyuri3@gmail.com>
2024-12-19 17:35:49 +0800

    few changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,11 +15,11 @@
     <section class="vh-100">
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                        class="img-fluid rounded-3 shadow-lg" alt="Sample image" />
+                <div class="col-md-9 col-lg-8 col-xl-5">
+                    <img src="{{ asset('images/logo.png') }}"
+                        class="img-fluid rounded-3 logo" alt="Sample image" />
                 </div>
-                <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <div class="col-md-8 col-lg-4 col-xl-4 offset-xl-1">
                     <!-- Login Form -->
                     <form action="{{ route('login.post') }}" method="post">
                         @csrf <!-- CSRF Token for security -->
@@ -57,9 +57,9 @@
                                     Select Role
                                 </button>
                                 <ul class="dropdown-menu" aria-labelledby="dropdownRoleButton">
-                                    <li><a class="dropdown-item" href="#" onclick="setRole('Admin')">Admin</a>
+                                    <li><a class="dropdown-item" onclick="setRole('Admin')">Admin</a>
                                     </li>
-                                    <li><a class="dropdown-item" href="#"
+                                    <li><a class="dropdown-item"
                                             onclick="setRole('Outsider')">Outsider</a></li>
                                 </ul>
                             </div>
